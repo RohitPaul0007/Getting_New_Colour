862fd5d9bfbd76f8d39b1e1e1f5682f92f8401d0
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-03 18:42:37 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function randomColor() {
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      let num = Math.floor(Math.random() * 16);
+      if (num < 10) {
+        color += num;
+      }
+      else {
+        switch (num) {
+          case 10:
+            color += "A";
+            break;
+          case 11:
+            color += "B";
+            break;
+          case 12:
+            color += "C";
+            break;
+          case 13:
+            color += "D";
+            break;
+          case 14:
+            color += "E";
+            break;
+          case 15:
+            color += "F";
+            break;
+        }
+      }
+    }
+    document.body.style.backgroundColor = color;
+    document.getElementById("color").innerHTML = color;
+  }
+  randomColor();
+  setInterval(randomColor, 1000);
